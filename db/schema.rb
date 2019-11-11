8909c5c5bbd48fa9954a29fe9edfcd234759989d
sumancrest0001 <suman.crest0001@gmail.com>
2019-11-12 00:04:11 +0545

    post model is created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_161543) do
+ActiveRecord::Schema.define(version: 2019_11_11_181759) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
