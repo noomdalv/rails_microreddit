0c6f6ca6c1d09068f0389c6f7b3170a75fe2e4de
noomdalv <vladlunasan@gmail.com>
2019-11-11 19:59:33 +0000

    added comment model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
+  
   validates :name, presence: true, length: { minimum: 4, maximum: 20 }
   validates :email, presence:true
 end
