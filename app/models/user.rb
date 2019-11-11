bbf7e14c8d30894ceba64a0c599bdc6f085dc789
noomdalv <vladlunasan@gmail.com>
2019-11-11 20:07:10 +0000

    added email regexp validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   has_many :posts
-  has_many :comments
+  has_many :comments, through => :posts
   
   validates :name, presence: true, length: { minimum: 4, maximum: 20 }
-  validates :email, presence:true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence:   true, length: { maximum: 255 },
+                    format:     { with: VALID_EMAIL_REGEX }
 end
