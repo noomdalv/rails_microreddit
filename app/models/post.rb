55b0aead86eb013c1be5ff42d034643070a459c8
noomdalv <vladlunasan@gmail.com>
2019-11-12 15:10:35 +0000

    added Post model validations, fixed User associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+  
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :body, presence: true, length: { maximum: 500 }
 end
